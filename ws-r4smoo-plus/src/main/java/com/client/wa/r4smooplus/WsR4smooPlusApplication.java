8dc7fb3987f15313f19f8d7b8b7cda76e8e0d4f1
Digpos <diegoc.sistemas@gmail.com>
2024-08-21 21:13:41 -0300

    First commit

---
+++ ws-r4smoo-plus/src/main/java/com/client/wa/r4smooplus/WsR4smooPlusApplication.java
@@ -0,0 +1,13 @@
+package com.client.wa.r4smooplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsR4smooPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsR4smooPlusApplication.class, args);
+	}
+
+}
---
+++ ws-r4smoo-plus/src/test/java/com/client/wa/r4smooplus/WsR4smooPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.wa.r4smooplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsR4smooPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
